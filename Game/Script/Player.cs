190167fdea05159e8d71224e18ac632db316f5ec
轩辕 <daiwenjun@bilibili.com>
2026-03-11 17:02:14 +0800

    feat: 完成动画

--- Game/Script/Player.cs
+++ Game/Script/Player.cs
@@ -1,31 +1,42 @@
-using Godot;
 using System;
+using Godot;
 
 public partial class Player : CharacterBody2D
 {
   public const float Speed = 150.0f;
   public const float ACCELERATE = 5.0f;
+  public string facingDirection = "Down";
+  AnimatedSprite2D animatedSprite2D;
+
+  public override void _Ready()
+  {
+	animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+  }
 
   public override void _PhysicsProcess(double delta)
   {
-    Vector2 velocity = Velocity;
+	Vector2 direction = Input.GetVector("Left", "Right", "Up", "Down");
 
-    // Add the gravity.
-    //if (!IsOnFloor())
-    //{
-    //velocity += GetGravity() * (float)delta;
-    //}
-
-    //// Handle Jump.
-    //if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-    //{
-    //velocity.Y = JumpVelocity;
-    //}
-
-    // Get the input direction and handle the movement/deceleration.
-    // As good practice, you should replace UI actions with custom gameplay actions.
-    Vector2 direction = Input.GetVector("Left", "Right", "Up", "Down");
-    Velocity = velocity.Lerp(direction * Speed, (float)(ACCELERATE * delta));
-    MoveAndSlide();
+	Velocity = Velocity.Lerp(direction * Speed, (float)(ACCELERATE * delta));
+	MoveAndSlide();
+	facingDirection = GetDirection(direction);
+	if (Velocity.Length() > 20)
+	  animatedSprite2D.Play("Run_" + facingDirection);
+	else
+	  animatedSprite2D.Play("Idle_" + facingDirection);
+  }
+  public string GetDirection(Vector2 direction)
+  {
+	if (direction == Vector2.Zero)
+	  return facingDirection;
+	if (direction.Y > 0)
+	  facingDirection = "Down";
+	else if (direction.Y < 0)
+	  facingDirection = "Up";
+	else if (direction.X > 0)
+	  facingDirection = "Right";
+	else if (direction.X < 0)
+	  facingDirection = "Left";
+	return facingDirection;
   }
 }
